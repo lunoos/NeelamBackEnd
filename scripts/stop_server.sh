097aa146a6446021ab113804d34e37855e6bc449
lunoos <mahendar9380@gmail.com>
2024-04-30 00:40:46 +0530

    updated stop_server.sh file

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,1 +1,11 @@
-kill $(jps | grep ecom-0.0.1-SNAPSHOT.war | awk '{print $1}')
+#!/bin/bash
+
+# Find the process ID (PID) of the Spring Boot application
+PID=$(pgrep -f 'ecom-0.0.1-SNAPSHOT.war')
+
+if [ -n "$PID" ]; then
+    echo "Stopping Spring Boot application with PID: $PID"
+    kill $PID
+else
+    echo "Spring Boot application is not running"
+fi
