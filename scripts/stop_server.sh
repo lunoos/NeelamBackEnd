a83b93cc4aed7d1b2b87b38914a3d4e2974bac67
lunoos <mahendar9380@gmail.com>
2024-05-01 16:53:20 +0530

    updated stop_server.sh file

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,7 +3,7 @@
 # Find the process ID (PID) of the running Spring Boot application
 SPRING_PID=$(ps -ef | grep "ecom-0.0.1-SNAPSHOT.war" | grep -v grep | awk '{print $2}')
 
-# If the process is running, stop it
+# If the process is running, stop the process
 if [ -n "$SPRING_PID" ]; then
     sudo kill -15 $SPRING_PID
 fi
