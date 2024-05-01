a721804d53b92dc1218777b2220da132f083dab3
lunoos <mahendar9380@gmail.com>
2024-05-01 15:41:26 +0530

    updated install_dependencies.sh file

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#delete war file
+# Set the path where you want to check for the WAR file
+war_file_path="/tmp/codedeploy-deployment-staging-area/ecom-0.0.1-SNAPSHOT.war"
+
+# Check if the WAR file exists
+if [ -f "$war_file_path" ]; then
+    sudo rm ecom-0.0.1-SNAPSHOT.war
+else
+    echo "WAR file not found at $war_file_path"
+fi
+
 # Install Java
 sudo yum install java-17-amazon-corretto-devel -y
 
